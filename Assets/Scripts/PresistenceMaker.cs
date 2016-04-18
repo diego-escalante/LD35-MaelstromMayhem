585bbc88cc3768ccde7ed778e95f3453a2fc7321
Diego Escalante <diego.escalante01@gmail.com>
2016-04-18 10:44:57 -0600

    Added death and spawn sounds

---
+++ Assets/Scripts/PresistenceMaker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PresistenceMaker : MonoBehaviour {
+
+  public GameObject ETrigger;
+  public GameObject Moosic;
+
+  private void Awake() {
+    GameObject g = GameObject.Find("Music Controller");
+    if(g == null) {
+      g = (GameObject)Instantiate(Moosic);
+      g.name = "Music Controller";
+    }
+
+    g = GameObject.Find("EventSystem");
+    if(g == null) {
+      g = (GameObject)Instantiate(ETrigger);
+      g.name = "EventSystem";
+    }
+  }
+}
